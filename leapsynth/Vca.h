5a27c9f3323e2b6bf0378c115569abdacb27eac0
Wiggins <wiggins@concentricsky.com>
2013-08-03 07:04:15 -0700

    basic vca

---
+++ leapsynth/Vca.h
@@ -0,0 +1,13 @@
+//
+//  Vca.h
+//  leapsynth
+//
+//  Created by Wiggins on 8/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Vca : NSObject
+
+@end
---
+++ leapsynth/Vca.m
@@ -0,0 +1,13 @@
+//
+//  Vca.m
+//  leapsynth
+//
+//  Created by Wiggins on 8/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "Vca.h"
+
+@implementation Vca
+
+@end
