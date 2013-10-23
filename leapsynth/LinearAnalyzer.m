414f1e8fbf66c8626dc7c391ba57e2093028164c
lithium <code@hlidskialf.com>
2013-10-23 14:15:04 -0700

    don't draw axis on linear analyzer

--- leapsynth/LinearAnalyzer.m
+++ leapsynth/LinearAnalyzer.m
@@ -9,6 +9,7 @@
 #import "LinearAnalyzer.h"
 
 @implementation LinearAnalyzer
+@synthesize drawAxis;
 
 - (id)initWithFrame:(NSRect)frame
 {
@@ -31,52 +32,48 @@
     [ctx saveGraphicsState];
     
     NSRect bounds = [self bounds];
-    
-//    [[NSColor blackColor] set];
-//    NSRectFill(bounds);
-    
-    // draw axis
-    NSBezierPath *minorAxisPath = [[NSBezierPath alloc] init];
-    [minorAxisPath setLineWidth:0.2];
-
-    NSBezierPath *majorAxisPath = [[NSBezierPath alloc] init];
-    [majorAxisPath setLineWidth:0.5];
-
-    
-    // draw minor axis ticks
-
-    int numTicks = 24;
-    double step = (bounds.size.height) / (bounds.size.height/16);
-    double j;
-    
-    for (j=0; j < bounds.size.height; j += step) {
-        [minorAxisPath moveToPoint:NSMakePoint(0,j)];
-        [minorAxisPath lineToPoint:NSMakePoint(bounds.size.width,j)];
         
-    }
-    step = (bounds.size.width) / numTicks;
-    for (j=0; j < bounds.size.width; j += step) {
-        [minorAxisPath moveToPoint:NSMakePoint(j,0)];
-        [minorAxisPath lineToPoint:NSMakePoint(j,bounds.size.height)];
-    }
-    [minorAxisColor set];
-    [minorAxisPath stroke];
+    // draw axis
+    if ([self drawAxis]) {
+        NSBezierPath *minorAxisPath = [[NSBezierPath alloc] init];
+        [minorAxisPath setLineWidth:0.2];
+        
+        NSBezierPath *majorAxisPath = [[NSBezierPath alloc] init];
+        [majorAxisPath setLineWidth:0.5];
 
-    // draw major axis
-    [majorAxisPath moveToPoint:NSMakePoint(0,bounds.size.height/2)];
-    [majorAxisPath lineToPoint:NSMakePoint(bounds.size.width,bounds.size.height/2)];
-//    [majorAxisPath moveToPoint:NSMakePoint(bounds.size.width/2,0)];
-//    [majorAxisPath lineToPoint:NSMakePoint(bounds.size.width/2,bounds.size.height)];
-    [majorAxisColor set];
-    [majorAxisPath stroke];
+        int numTicks = 24;
+        double step = (bounds.size.height) / (bounds.size.height/16);
+        double j;
+        
+        // draw minor axis ticks
+        for (j=0; j < bounds.size.height; j += step) {
+            [minorAxisPath moveToPoint:NSMakePoint(0,j)];
+            [minorAxisPath lineToPoint:NSMakePoint(bounds.size.width,j)];
+            
+        }
+        step = (bounds.size.width) / numTicks;
+        for (j=0; j < bounds.size.width; j += step) {
+            [minorAxisPath moveToPoint:NSMakePoint(j,0)];
+            [minorAxisPath lineToPoint:NSMakePoint(j,bounds.size.height)];
+        }
+        [minorAxisColor set];
+        [minorAxisPath stroke];
 
+        // draw major axis
+        [majorAxisPath moveToPoint:NSMakePoint(0,bounds.size.height/2)];
+        [majorAxisPath lineToPoint:NSMakePoint(bounds.size.width,bounds.size.height/2)];
+    //    [majorAxisPath moveToPoint:NSMakePoint(bounds.size.width/2,0)];
+    //    [majorAxisPath lineToPoint:NSMakePoint(bounds.size.width/2,bounds.size.height)];
+        [majorAxisColor set];
+        [majorAxisPath stroke];
+    }
 
     // draw waveform
     NSBezierPath *wavePath = [[NSBezierPath alloc] init];
     [wavePath setLineWidth:3.0];
     [wavePath moveToPoint:NSMakePoint(0,bounds.size.height/2)];
     
-    step = bounds.size.width/samplesInBuffer;
+    double step = bounds.size.width/samplesInBuffer;
     int i;
     short *samples = (short*)[buffer bytes];
     for (i=0; i < samplesInBuffer; i++) {
